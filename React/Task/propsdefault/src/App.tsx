34264d473563f1928fb07937d77d8458a3ac5db8
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-11-26 18:55:41 +0530

    saving current chages

--- React/Task/propsdefault/src/App.tsx
+++ React/Task/propsdefault/src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
+// import "./App.css";
 
-import { Card } from './components/Card';
+// import { Card } from './Components/Card.jsx';
 
-export const App = () => {
-  return(
-    <>
-    <h1>Hello</h1>
-    <div style={{ display: 'flex', gap: '10px'}}>
-      <Card name={'Afan'} age={20} email={'shaikhaffan037@gmail.com'} />
-      <Card name={'Luca'} age={20} email={'luca037@gmail.com'} />
-      <Card/>
-    </div>
-    </>
-  );
-};
+// export const App = () => {
+//   return (
+//     <>
+//       <h1>Hello</h1>
+//       <div style={{ display: "flex", gap: "10px" }}>
+//         <Card name={"Afan"} age={20} email={"shaikhaffan037@gmail.com"} />
+//         <Card name={"Luca"} age={20} email={"luca037@gmail.com"} />
+//         <Card />
+//       </div>
+//     </>
+//   );
+// };
