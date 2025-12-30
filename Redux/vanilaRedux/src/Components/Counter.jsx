ce011c7d0de4d313495286b3ad83d5dd91ad8365
AfanShaikh <shaikhaffan.8286@gmail.com>
2025-12-30 17:40:05 +0530

    code

--- Redux/vanilaRedux/src/Components/Counter.jsx
+++ Redux/vanilaRedux/src/Components/Counter.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
+
+import { myStore } from '../Store/Store';
+import * as types from '../Reducer/Action';
 
 export const Counter = () => {
-    const [ count, setCount ] = React.useState(0);
+    // const [ count, setCount ] = React.useState(0);
+    const [jugad, setJugad] = React.useState(0);
+
+    const { getState, dispatch, subscribe } = myStore;
+
+    subscribe(() => {
+      setJugad((prev)=> prev +1);
+    });
 
 
 
   return (
     <>
-        <h1>Counter{count}</h1>
-        <button onClick={()=>setCount((prev) => prev+1)}>+</button>
-        <button onClick={()=>setCount((prev) => prev-1)}>-</button>
+        <h1>Counter{getState().count}</h1>
+        <button onClick={()=> dispatch({type: types.INCREMENT})}>+</button>
+        <button onClick={()=> dispatch({type: types.DECREMENT})}>-</button>
     </>
   );
 };
