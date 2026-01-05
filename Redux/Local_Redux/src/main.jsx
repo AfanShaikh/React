77f61bcafbf37edf372bb7782094efde31d7f4cf
AfanShaikh <shaikhaffan.8286@gmail.com>
2026-01-05 17:41:38 +0530

    code of the day

---
+++ Redux/Local_Redux/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './App.css'
+
+import { Provider } from 'react-redux';
+import { myOwnStore } from './App/Store.js';
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={myOwnStore}>
+    <App />
+  </Provider>
+);
